8d9ec13b460faeac871e0bf26a316a824898adef
Glory <glorylion07@gmail.com>
2026-08-19 17:15:11 +0800

    Update echo to echo user commands, exit when bye

--- src/main/java/Lion.java
+++ src/main/java/Lion.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lion {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
@@ -5,7 +7,7 @@ public class Lion {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "________________________________";
+        String line = " ________________________________";
         System.out.println(banner);
         System.out.println(line);
         System.out.println("Hello! I'm Lion.");
@@ -13,5 +15,20 @@ public class Lion {
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
+        System.out.println("    What can I do for you?");
+        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(line);
+            System.out.println("    " + input);
+            System.out.println(line);
+            input = scanner.nextLine();
+        }
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
+
     }
 }

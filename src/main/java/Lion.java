62637768291638c118f9236a1e48ba3d934c9ec4
Glory <glorylion07@gmail.com>
2026-08-19 17:01:33 +0800

    Rename bot to Lion and add greeting

---
+++ src/main/java/Lion.java
@@ -0,0 +1,17 @@
+public class Lion {
+    public static void main(String[] args) {
+        String banner = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "________________________________";
+        System.out.println(banner);
+        System.out.println(line);
+        System.out.println("Hello! I'm Lion.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}

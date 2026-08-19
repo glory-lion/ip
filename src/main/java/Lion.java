adac894a2099cc9abc458605d9bded724ed2f529
Glory <glorylion07@gmail.com>
2026-08-19 19:53:46 +0800

    Add proper indentation

--- src/main/java/Lion.java
+++ src/main/java/Lion.java
@@ -10,19 +10,17 @@ public class Lion {
         String line = " ________________________________";
         System.out.println(banner);
         System.out.println(line);
-        System.out.println("Hello! I'm Lion.");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println("    Hello! I'm Lion.");
         System.out.println("    What can I do for you?");
         System.out.println(line);
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
             System.out.println(line);
             System.out.println("    " + input);
             System.out.println(line);
+
             input = scanner.nextLine();
         }
         System.out.println(line);

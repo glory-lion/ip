1a35b4b964512cbf19e3dd5ad24ac477a0b99679
Glory <glorylion07@gmail.com>
2026-08-19 23:22:43 +0800

    Add Task class

--- src/main/java/Lion.java
+++ src/main/java/Lion.java
@@ -14,8 +14,7 @@ public class Lion {
         System.out.println("    What can I do for you?");
         System.out.println(line);
 
-        String[] list = new String[100];
-        boolean[] isDone = new boolean[100];
+        Task[] list = new Task[100];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
@@ -25,29 +24,28 @@ public class Lion {
             if(input.equals("list")) {
                 System.out.println("    Here are the tasks in your list:");
                 for(int i = 0; i < taskCount; i++) {
-                    String status = isDone[i] ? "X" : " ";
-                    System.out.println("    " + (i + 1) + ".[" + status + "] " + list[i]);
+                    System.out.println("    " + (i + 1) + ".["  + list[i].getStatusIcon() + "] " + list[i].getDescription());
                 }
             }
             else if(input.startsWith("mark")) {
                 String number = input.substring(5);
                 int taskNumber = Integer.parseInt(number) - 1;
-                isDone[taskNumber] = true;
+                list[taskNumber].markAsDone();
 
                 System.out.println("    Nice! I've marked this task as done:");
-                System.out.println("     [X] " + input);
+                System.out.println("     [X] " + list[taskNumber].getDescription());
 
             }
             else if(input.startsWith("unmark")) {
                 String number = input.substring(7);
                 int taskNumber = Integer.parseInt(number) - 1;
-                isDone[taskNumber] = false;
+                list[taskNumber].markAsNotDone();
 
                 System.out.println("    OK! I've marked this task as not done yet:");
-                System.out.println("     [ ] " + input);
+                System.out.println("     [ ] " + list[taskNumber].getDescription());
             }
             else {
-                list[taskCount] = input;
+                list[taskCount] = new Task(input);
                 taskCount++;
 
                 System.out.println("    added: " + input);
@@ -56,7 +54,7 @@ public class Lion {
             input = scanner.nextLine();
         }
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println(line);
 
 

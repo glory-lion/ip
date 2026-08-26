b016f72d9b2b922349583b4edd786d269cb823ed
Glory <glorylion07@gmail.com>
2026-08-27 00:32:58 +0800

    Refactor Lion to use more OOP

---
+++ src/main/java/Parser.java
@@ -0,0 +1,31 @@
+public class Parser {
+    public CommandType getCommandType(String input) {
+        return CommandType.from(input);
+    }
+
+    public String getTodoDescription(String input) {
+        return input.substring(4).trim();
+    }
+
+    public String[] getDeadlineParts(String input) {
+        String details = input.substring(9);
+        return details.split(" /by ", 2);
+    }
+
+    public String[] getEventParts(String input) {
+        String details = input.substring(5).trim();
+        String[] fromParts = details.split(" /from ", 2);
+        String[] toParts = fromParts[1].split(" /to ", 2);
+
+        String description = fromParts[0];
+        String from = toParts[0];
+        String to = toParts[1];
+
+        return new String[] { description, from, to };
+    }
+
+    public int getTaskIndex(String input, int prefixLength) {
+        String number = input.substring(prefixLength).trim();
+        return Integer.parseInt(number) - 1;
+    }
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+
+public class TaskList {
+    private final Task[] tasks;
+    private int size;
+
+    public TaskList() {
+        this.tasks = new Task[100];
+        this.size = 0;
+    }
+
+    public TaskList(Task[] tasks, int size) {
+        this.tasks = tasks;
+        this.size = size;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Task get(int index) {
+        return tasks[index];
+    }
+
+    public void add(Task task) {
+        tasks[size] = task;
+        size++;
+    }
+
+    public Task delete(int index) {
+        Task deleted = tasks[index];
+        for(int i = index; i < size - 1; i++) {
+            tasks[i] = tasks[i + 1];
+        }
+        size--;
+        tasks[size] = null;
+        return deleted;
+    }
+
+    public void mark(int index) {
+        tasks[index].markAsDone();
+    }
+
+    public void unmark(int index) {
+        tasks[index].markAsNotDone();
+    }
+
+    public void save() throws IOException {
+        Storage.save(tasks, size);
+    }
+
+}

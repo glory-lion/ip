2f117f1116fe8c0dfa11df894efc76afa38be9ab
Glory <glorylion07@gmail.com>
2026-08-27 00:32:58 +0800

    Refactor Lion to use more OOP

---
+++ src/main/java/Ui.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ui {
+    private final Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome(String banner, String line) {
+        System.out.println(banner);
+        System.out.println(line);
+        System.out.println("    Hello! I'm Lion.");
+        System.out.println("    What can I do for you?");
+        System.out.println(line);
+    }
+
+    public void showLine(String line) {
+        System.out.println(line);
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showGoodbye(String line) {
+        System.out.println(line);
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}

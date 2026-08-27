c38e716613f19607d4b914ade75d1a4a8563c8c5
Glory <glorylion07@gmail.com>
2026-08-27 17:38:37 +0800

    Add JavaDoc comments

--- src/main/java/lion/Todo.java
+++ src/main/java/lion/Todo.java
@@ -1,9 +1,23 @@
 package lion;
 
+/**
+ * Represents a task with a description and no date or time details.
+ */
 public class Todo extends Task {
+    /**
+     * Creates an incomplete todo task.
+     *
+     * @param description description of the task
+     */
     public Todo(String description) {
         super(description);
     }
+
+    /**
+     * Returns the one-letter icon used to identify todo tasks.
+     *
+     * @return {@code T}
+     */
     @Override
     public String getTypeIcon() {
         return "T";
